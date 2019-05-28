95b423936972274cb99397c8657cc023cf842b4f
saylestyler <saylestyler@gmail.com>
2019-05-28 15:28:33 -0400

    standard

--- client/src/GoogleMap.js
+++ client/src/GoogleMap.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import {
+  Map,
+  //InfoWindow,
+  Marker,
+  GoogleApiWrapper
+} from 'google-maps-react';
 
 export class MapContainer extends Component {
+
   render() {
     return (
-      <Map google={this.props.google} zoom={14}>
-
-        <Marker onClick={this.onMarkerClick}
-          name={'Current location'} />
-
-        <InfoWindow onClose={this.onInfoWindowClose}>
-          <div>
-            <h1>{this.state.selectedPlace.name}</h1>
-          </div>
-        </InfoWindow>
+      <Map google={this.props.google}
+        style={{ width: '100%', height: '100%', position: 'relative' }}
+        className={'map'}
+        zoom={14}>
+        <Marker
+          title={'The marker`s title will appear as a tooltip.'}
+          name={'SOMA'}
+          position={{ lat: 37.778519, lng: -122.405640 }} />
+        <Marker
+          name={'Dolores park'}
+          position={{ lat: 37.759703, lng: -122.428093 }} />
+        <Marker />
+        <Marker
+          name={'Your position'}
+          position={{ lat: 37.762391, lng: -122.439192 }}
+          icon={{
+            url: "/path/to/custom_icon.png",
+            anchor: new google.maps.Point(32, 32),
+            scaledSize: new google.maps.Size(64, 64)
+          }} />
       </Map>
     );
   }
 }
+
 
 export default GoogleApiWrapper({
   apiKey: (`AIzaSyCrcRtbzRXLRKVG8JlcaEJTnC4GIWCaShU`)
