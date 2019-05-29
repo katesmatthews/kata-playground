32164a2434c1ad158fa06ee6cf12cc428e412075
saylestyler <saylestyler@gmail.com>
2019-05-29 10:06:41 -0400

    pw

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require('path');
 const generatePassword = require('password-generator');
-require('dotenv').config()
-
 
 const app = express();
 
@@ -27,7 +25,7 @@ app.get('/api/passwords', (req, res) => {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
