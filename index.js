f85a6d59460ebebe664b5003327e2fee973952d3
saylestyler <saylestyler@gmail.com>
2019-05-29 09:39:03 -0400

    bloop

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const generatePassword = require('password-generator');
 
+
 const app = express();
 
 // Serve static files from the React app
